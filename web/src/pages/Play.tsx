eac4c8f139c76cffe15e87e68924ed2dd440de51
Austin Smith <pairjax@protonmail.com>
2022-10-24 10:15:47 -0400

    Accept MIDI Input
    - System now accepts MIDI input for playing/stopping note.
    - Does not do anything with this input yet.

--- web/src/pages/Play.tsx
+++ web/src/pages/Play.tsx
@@ -28,10 +28,37 @@ export default function Play() {
 
     function onMIDISuccess(midiAccess: WebMidi.MIDIAccess) {
         console.log('MIDI Access Object: ' + midiAccess)
+
+        for (let input of midiAccess.inputs.values()) {
+            input.onmidimessage = getMIDIMessage;
+        }
     }
 
     function onMIDIFailure(e: Error) {
         console.log('MIDI Access Failed! Error: ' + e)
+    }
+
+    function getMIDIMessage(message: WebMidi.MIDIMessageEvent) {
+        let command = message.data[0];
+        let note = message.data[1];
+        let velocity = (message.data.length > 2) ? message.data[2] : 0;
+
+        switch (command) {
+            case 144: // noteOn
+                noteOn(note, velocity);
+            case 128: // noteOff
+                noteOff(note);
+            default:
+                console.warn("This MIDI Input is not supported yet!");
+        }
+    }
+
+    function noteOn(note: number, velocity: number) {
+
+    }
+
+    function noteOff(note: number) {
+
     }
 
     return (
