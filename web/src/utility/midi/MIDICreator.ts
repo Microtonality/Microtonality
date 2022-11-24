b64822510565708904047b397c365e5fc1956c62
Manierre-nathan <Manierre.Nathan@knights.ucf.edu>
2022-11-23 19:04:44 -0500

    Added support for MIDI creation.

--- web/src/utility/midi/MIDICreator.ts
+++ web/src/utility/midi/MIDICreator.ts
@@ -1,12 +1,17 @@
-// TODO: Create MIDI from button inputs by user.
+import { onMIDIMessage } from './MIDIReceiver';
 
-// Example Code
-// Note C: 144 128 64
+// supported notes
+const Notes = [261.6, 277.2, 293.7, 311.1, 329.6, 349.2, 370, 392, 415.3, 440, 466.2, 493.9, 523.3]
 
-/*OnNotePressed(Note) {
-    if (Note is C) {
-        let note: WebMidi.MIDIMessageEvent;
-        note = 144 128 64;
-        MIDIReceiver.onmidimessage(note);
+function OnNotePressed(note: number) {
+    let message: WebMidi.MIDIMessageEvent;
+    if (Notes.includes(note)){
+        message.data[0] = 144; // noteOn
+        message.data[1] = note; // pitch
+        message.data[2] = 60;   // velocity
+        onMIDIMessage(message);
     }
-}*/
+    else {
+        console.error("the note: " + note + " is not supported")
+    }
+}
