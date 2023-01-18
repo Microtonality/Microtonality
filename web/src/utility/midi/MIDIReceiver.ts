a4ca4b671124016a6707faacfd250763426ea133
Manierre-nathan <55036068+Manierre-nathan@users.noreply.github.com>
2023-01-18 16:16:37 -0500

    MIDI unit testing
    Add MIDI testing files. Change MIDIReceiver to class, make changes in MIDICreator for compatibility.

--- web/src/utility/midi/MIDIReceiver.ts
+++ web/src/utility/midi/MIDIReceiver.ts
@@ -1,66 +1,69 @@
-let midiInput: WebMidi.MIDIInput[] = [];
-let midiOutput: WebMidi.MIDIOutput[] = [];
+export class MIDIReceiver {
 
-const NOTE_ON_MESSAGE = 144;
-const NOTE_OFF_MESSAGE = 128;
-const PITCH_BEND_MESSAGE = 224;
+    public midiInput: WebMidi.MIDIInput[] = [];
+    public midiOutput: WebMidi.MIDIOutput[] = [];
 
-export function connectToInstrument() {
-    navigator.requestMIDIAccess().then(
-        (midi) => initDevices(midi),
-        (err) => console.warn('unable to connect to midi', err)
-    )
-}
+    private static NOTE_ON_MESSAGE: number = 144;
+    private static NOTE_OFF_MESSAGE: number = 128;
+    private static PITCH_BEND_MESSAGE: number = 224;
 
-function initDevices(midi: WebMidi.MIDIAccess) {
-    midiInput = []
-    midiOutput = []
-
-    for (let input of midi.inputs.values()) {
-        midiInput.push(input)
-        input.onmidimessage = onMIDIMessage
+    public connectToInstrument() {
+        navigator.requestMIDIAccess().then(
+            (midi) => this.initDevices(midi),
+            (err) => console.warn('unable to connect to midi', err)
+        )
     }
 
-    for (let output of midi.outputs.values()) {
-        midiOutput.push(output)
+    public initDevices(midi: WebMidi.MIDIAccess) {
+        this.midiInput = []
+        this.midiOutput = []
+
+        for (let input of midi.inputs.values()) {
+            this.midiInput.push(input)
+            input.onmidimessage = this.onMIDIMessage
+        }
+
+        for (let output of midi.outputs.values()) {
+            this.midiOutput.push(output)
+        }
     }
-}
 
-function onMIDIMessage(message: WebMidi.MIDIMessageEvent) {
-    let command = message.data[0]
+    public onMIDIMessage(message: WebMidi.MIDIMessageEvent) {
+        let command = message.data[0]
 
-    if (command == NOTE_ON_MESSAGE) {
-        let note = message.data[1]
-        let velocity = message.data[2]
+        if (command == MIDIReceiver.NOTE_ON_MESSAGE) {
+            let note = message.data[1]
+            let velocity = message.data[2]
 
-        noteOn(note, velocity)
-    } else if (command == NOTE_OFF_MESSAGE) {
-        let note = message.data[1]
+            this.noteOn(note, velocity)
+        } else if (command == MIDIReceiver.NOTE_OFF_MESSAGE) {
+            let note = message.data[1]
 
-        noteOff(note)
-    } else if (command == PITCH_BEND_MESSAGE) {
-        let finePitchBend = message.data[1]
-        let coarsePitchBend = message.data[2]
+            this.noteOff(note)
+        } else if (command == MIDIReceiver.PITCH_BEND_MESSAGE) {
+            let finePitchBend = message.data[1]
+            let coarsePitchBend = message.data[2]
 
-        setGlobalPitchBend(finePitchBend, coarsePitchBend)
-    } else {
-        console.warn("This MIDI Input is not supported yet!")
+            this.setGlobalPitchBend(finePitchBend, coarsePitchBend)
+        } else {
+            console.warn("This MIDI Input is not supported yet!")
+        }
     }
-}
 
-export function noteOn(note: number, velocity: number) {
-    // Find correct frequency for note
-    let frequency = note;
+    public noteOn(note: number, velocity: number) {
+        // Find correct frequency for note
+        let frequency = note;
 
-    // Send new note w/ velocity to sound generator
-}
+        // Send new note w/ velocity to sound generator
+    }
 
-export function noteOff(note: number) {
-    // Fidn correct frequency for note
+    public noteOff(note: number) {
+        // Fidn correct frequency for note
 
-    // Find new note to disable.
-}
+        // Find new note to disable.
+    }
 
-export function setGlobalPitchBend(finePitchBend: number, coarsePitchBend: number) {
-    // Probably move this to another class specifically for pitch bend calculation
+    public setGlobalPitchBend(finePitchBend: number, coarsePitchBend: number) {
+        // Probably move this to another class specifically for pitch bend calculation
+    }
 }
