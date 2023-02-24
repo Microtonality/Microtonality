e7b1b3781f589b2609cb8d8e005fb59a70030d0f
Austin Smith <pairjax@protonmail.com>
2023-02-24 10:48:46 -0500

    Small Formatting

--- web/src/ui/Oscillator.tsx
+++ web/src/ui/Oscillator.tsx
@@ -3,20 +3,20 @@ import {FormControl, Grid, Slider} from "@mui/material";
 
 export default function Oscillator() {
     return (
-            <div className='silent'>
-                <Slider
-                    sx={{
-                    '& input[type="range"]': {
-                        WebkitAppearance: 'slider-vertical',
-                    },
-                        color: 'white'
-                }}
-                    id="pitch_ratio"
-                    orientation="vertical"
-                    defaultValue={0}
-                    aria-label="Slider2"
-                    valueLabelDisplay="auto"
-                />
-            </div>
-            )
+        <div className='silent'>
+            <Slider
+                sx={{
+                '& input[type="range"]': {
+                    WebkitAppearance: 'slider-vertical',
+                },
+                    color: 'white'
+            }}
+                id="pitch_ratio"
+                orientation="vertical"
+                defaultValue={0}
+                aria-label="Slider2"
+                valueLabelDisplay="auto"
+            />
+        </div>
+    )
 }
