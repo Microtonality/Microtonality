969650266860b7f9b43fdac7cc2c5e9e3984b28d
Peter DeVita <mewtwo2643@yahoo.com>
2023-03-09 17:45:53 -0500

    Fix default rootKey (should be +1 I think?)

--- web/src/utility/MicrotonalConfig.tsx
+++ web/src/utility/MicrotonalConfig.tsx
@@ -52,7 +52,7 @@ export const DEFAULT_SYNTH_CONFIG: SynthConfig = {
 export const DEFAULT_SCALE_CONFIG: ScaleConfig = {
     keysPerOctave: 12,
     tuningFrequency: 440,
-    rootKey: 69,
+    rootKey: 70,
     scale: generateEqualTemperedScale(12)
 }
 
