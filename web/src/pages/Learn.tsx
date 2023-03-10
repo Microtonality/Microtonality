1ec932f2486f4cfb2bf20a7e9e37526cae61832e
ZH773 <111894882+ZH773@users.noreply.github.com>
2023-03-10 16:16:38 -0500

    Font color is now black for Learn page

--- web/src/pages/Learn.tsx
+++ web/src/pages/Learn.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 export function Learn() {
     return (
         
-        <div className={"text-white px-16 py-8 text-left text-base"}> This project works on multiple fronts to bring microtonal works to life. As a starting point,
+        <div className={"text-black px-16 py-8 text-left text-base"}> This project works on multiple fronts to bring microtonal works to life. As a starting point,
         Easley Blackwood's microtonal etudes provide a number of difficulties to overcome in order to
         produce a full performance. You can read about the ideas to tackle these challenges in the
         paper below.
