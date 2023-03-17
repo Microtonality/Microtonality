8ecc642357d464423dee93625eda6a493264f3cd
Calvin Agar <calvinlagar@gmail.com>
2023-03-17 13:49:28 -0400

    small knob fix
    negated the y-value in the return statement because the MouseEvent's coordinate system's y-value increases as you move down.
    now calling functions in divs with arrow functions

--- web/src/ui/Knobs.tsx
+++ web/src/ui/Knobs.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Component, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface KnobProps {
   min: number;
@@ -54,7 +54,7 @@ export default function Knob (props: KnobProps) {
   const handleMouseMove = (event: MouseEvent) => {
     const { movementX, movementY } = event;
     setValue((prevValue) => {
-      return Math.min(props.max, Math.max((movementX/(100/props.max)) + (movementY/(100/props.max)) + prevValue, props.min));
+      return Math.min(props.max, Math.max(props.min, (movementX/(100/props.max)) + (-1)*(movementY/(100/props.max)) + prevValue));
     });
   };
 
@@ -72,15 +72,13 @@ export default function Knob (props: KnobProps) {
   }
 
     return (
-
-      
       <div className={`flex flex-col min-h-[96%] w-full justify-around items-center bg-neutral-700 rounded-xl ${props.className}`}>
 
         <div className="flex w-[7rem] justify-center">
           <a className="2xl:text-xl xl:text-lg md:text-sm sm:text-xs font-agrandir-wide uppercase px-5 py-3 rounded block leading-normal text-center text-white">{props.knobLabel}</a>
         </div>
 
-        <div className={"aspect-square rounded-full p-2 bg-gradient-to-b from-neutral-500 to-neutral-900 flex w-3/4"} onMouseDown={handleMouseDown}>
+        <div className={"aspect-square rounded-full p-2 bg-gradient-to-b from-neutral-500 to-neutral-900 flex w-3/4"} onMouseDown={(e) => handleMouseDown(e)}>
           <div className={"w-full h-full rounded-full p-0.5 bg-gradient-to-b from-stone-700 to-neutral-800 flex-1"} >
             <div style={{transform: `rotate(${valueToAngle(value)}deg)`}} className={"w-full h-full flex items-start justify-center"}>
               <div className={"w-2.5 h-2.5 rounded-full bg-black"}>
@@ -89,10 +87,9 @@ export default function Knob (props: KnobProps) {
           </div>
         </div>
 
-        <input className={"text-center self-center w-3/4 rounded-md font-agrandir"} type="number" value={parseFloat(value.toFixed(2))} onChange={handleInput} min={0} max={1} step={0.01} />
+        <input className={"text-center self-center w-3/4 rounded-md font-agrandir"} type="number" value={parseFloat(value.toFixed(2))} onChange={(e) => handleInput(e)} min={0} max={1} step={0.01} />
       </div>
     );
-  
 }
 
 Knob.defaultProps = defaultProps;
