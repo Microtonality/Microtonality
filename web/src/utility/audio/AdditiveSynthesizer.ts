5dc71567c0c73694ba94a4c81534a56552f18770
Austin Smith <pairjax@protonmail.com>
2023-04-21 10:09:12 -0400

    Notes Additive Synthesizer & Oscillators

--- web/src/utility/audio/AdditiveSynthesizer.ts
+++ web/src/utility/audio/AdditiveSynthesizer.ts
@@ -1,26 +1,41 @@
 import {SynthConfig, DEFAULT_SYNTH_CONFIG} from "../MicrotonalConfig";
 import OscillatorStack from "./OscillatorStack";
 
+/**
+ * Manages the creation of audio within the application.
+ * Creates a stack of oscillators when playing a new note.
+ * Oscillator stacks are mapped to the frequency they were
+ * played at in a dictionary.
+ *
+ * The audio context, master gain and dynamics compressor
+ * are a part of the Web Audio API that all oscillators
+ * transmit information to in order to generate sound.
+ */
 export class AdditiveSynthesizer {
     config: SynthConfig = DEFAULT_SYNTH_CONFIG;
 
+    /** Controls sound generation and connects to every oscillator */
     audioContext: AudioContext = new AudioContext()
+    /** Controls the volume of the sounds generated globally. Linked to the main gain knob */
     masterGain: GainNode = this.audioContext.createGain()
+    /** Controls the sound to prevent it from peaking & creating harsh clipping */
     dynamicsCompressor: DynamicsCompressorNode = this.audioContext.createDynamicsCompressor()
 
+    /** Dictinoary holding oscillator stacks whose key is the base frequency being played */
     oscillatorStacks: { [key: number]: OscillatorStack } = {}
 
     constructor() {
-
         this.dynamicsCompressor.connect(this.masterGain)
         this.masterGain.connect(this.audioContext.destination)
     }
 
+    /** Called whenever the synth config is updated. Updates master gain & stops all current oscillators. */
     updateSettings() {
         this.masterGain.gain.value = this.config.gain
         this.clearOscillators()
     }
 
+    /** Stops all current oscillators and removes them from the dictionary. */
     clearOscillators() {
         for (let frequency in this.oscillatorStacks) {
             this.oscillatorStacks[frequency].endPlay(this.audioContext, this.config.release)
@@ -28,6 +43,11 @@ export class AdditiveSynthesizer {
         }
     }
 
+    /**
+     * Creates a new oscillator stack at the frequency being played.
+     * @param {number} frequency - The base frequency of the stack.
+     * @param {number} velocity - The intensity of the note. Not yet implemented.
+     */
     onPlayFrequency(frequency: number, velocity: number) {
         if (this.oscillatorStacks[frequency] != null) {
             return;
@@ -44,6 +64,10 @@ export class AdditiveSynthesizer {
         this.oscillatorStacks[frequency].beginPlay(this.audioContext, this.config)
     }
 
+    /**
+     * Stops an oscillator stack & removes it from the dictinary
+     * @param {number} frequency - The base frequency of the stack being removed.
+     */
     onStopFrequency(frequency: number) {
         if (this.oscillatorStacks[frequency] == null) {
             return;
--- web/src/utility/audio/OscillatorStack.ts
+++ web/src/utility/audio/OscillatorStack.ts
@@ -17,6 +17,7 @@ export default class OscillatorStack {
             ) {
         for (let i = 0; i < oscillatorSettings.length; i++) {
             this.oscillatorNodes[i] = audioContext.createOscillator()
+            // Set oscillator frequency relative to the base frequency
             this.oscillatorNodes[i].frequency.setValueAtTime(
                     oscillatorSettings[i].pitchRatio * frequency,
                     audioContext.currentTime,
@@ -24,15 +25,21 @@ export default class OscillatorStack {
 
             this.oscillatorNodes[i].type = oscillatorSettings[i].waveType;
 
+            // Create local gain for this oscillator & make it silent
             this.gainNodes[i] = audioContext.createGain()
             this.gainNodes[i].gain.value = 0
 
+            // Connect the gain nodes to the oscillators & then to the compressor.
             this.oscillatorNodes[i].connect(this.gainNodes[i])
             this.gainNodes[i].connect(dynamicsCompressor)
         }
     }
 
+    /**
+     * Begins sound generation for the given stack.
+     */
     async beginPlay(audioContext: AudioContext, config: SynthConfig) {
+        // Start sound & ramp to max volume for each oscillator
         for (let i = 0; i < this.oscillatorNodes.length; i++) {
             this.oscillatorNodes[i].start(0)
             this.gainNodes[i].gain.setValueAtTime(0, 0)
@@ -41,8 +48,10 @@ export default class OscillatorStack {
                     audioContext.currentTime + (config.attack))
         }
 
+        // Wait until attack time period is done
         await new Promise(f => setTimeout(f, config.attack * 1001))
 
+        // If the user hasn't released the note, ramp down to the sustain level.
         if (!this.endingPlay) {
             for (let i = 0; i < this.oscillatorNodes.length; i++) {
                 this.gainNodes[i].gain.linearRampToValueAtTime(
@@ -52,18 +61,26 @@ export default class OscillatorStack {
         }
     }
 
+    /**
+     * Begins release envelope & ends sound generation for the given stack.
+     */
     async endPlay(audioContext: AudioContext, release: number) {
+        // Flag so note doesn't sustain when ending.
         this.endingPlay = true
 
+        // Ramp down to silence
         this.gainNodes.forEach(function (gain) {
+            // Force the note to the exact volume it should be before ramping down. Prevents weird audio bugs.
             gain.gain.linearRampToValueAtTime(gain.gain.value, audioContext.currentTime + 0.001)
             gain.gain.linearRampToValueAtTime(0.0001, audioContext.currentTime + (release))
         })
 
+        // Wait until release time interval is done
         await new Promise(f => setTimeout(f, release * 1000))
 
         this.stopped = true
 
+        // Stop all oscillators
         this.oscillatorNodes.forEach(function (oscillator) {
             oscillator.stop(1)
         })
